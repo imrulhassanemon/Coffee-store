9dd677e91b937bba942c5c0e0ce184e85a148a3f
imrulhassanemon <imrulhassan989@gmail.com>
2025-11-05 09:50:31 -0800

    Coffee store

---
+++ coffee-store-server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 5000;
+
+
+// MIDDLEWARE 
+
+app.use(cors())
+app.use(express.json())
+
+
+
+app.get('/', (req, res) =>{
+    res.send("Coffee Server Is Running.")
+})
+
+app.listen(port, ()=>{
+    console.log(`Coffee Server is Running On PORT: ${port}`)
+    
+})
+
+
+
