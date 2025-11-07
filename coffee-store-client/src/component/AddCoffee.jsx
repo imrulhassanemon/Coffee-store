824e7c9553f1525b2a8416d90330e5b1ffcf0572
imrulhassanemon <imrulhassan989@gmail.com>
2025-11-07 11:39:35 -0800

    added coffee to the server

--- coffee-store-client/src/component/AddCoffee.jsx
+++ coffee-store-client/src/component/AddCoffee.jsx
@@ -1,27 +1,49 @@
 import React from "react";
 import {FaArrowCircleLeft} from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AddCoffee = () => {
-
-  const handelSubmit = (event)=>{
-    event.preventDefault()
+  const handelSubmit = (event) => {
+    event.preventDefault();
     const form = event.target;
     const name = form.name.value;
     const chef = form.chef.value;
-    const supplier = form.supplier.value
+    const supplier = form.supplier.value;
     const taste = form.taste.value;
     const category = form.category.value;
-    const details   = form.details.value;
+    const details = form.details.value;
     const photo = form.photo.value;
-    const coffee = {name:name, chef:chef, supplier:supplier, taste:taste, category:category, details:details, photo:photo}
+    const coffee = {
+      name: name,
+      chef: chef,
+      supplier: supplier,
+      taste: taste,
+      category: category,
+      details: details,
+      photo: photo,
+    };
 
+    fetch("http://localhost:5000/coffee", {
+      method: "POST",
+      headers: {"content-type": "application/json"},
+      body: JSON.stringify(coffee),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Good job!",
+            text: "You successfully added user!",
+            icon: "success",
+          });
+          form.reset()
+        }
+      });
 
     console.log(coffee);
-    
-  }
-
-
-
+  };
 
   return (
     // <div className="flex justify-center h-screen items-center border border-red-600">
@@ -39,51 +61,88 @@ const AddCoffee = () => {
       </div>
       <form onSubmit={handelSubmit}>
         <div className="grid md:grid-cols-12 gap-5 p-5 w-full border border-amber-400">
-          <div className='md:col-span-6'>
+          <div className="md:col-span-6">
             <fieldset className="fieldset">
               <legend className="fieldset-legend">Name:</legend>
-              <input type="text" name="name" className="input w-full" placeholder="Enter your name" />
+              <input
+                type="text"
+                name="name"
+                className="input w-full"
+                placeholder="Enter your name"
+              />
             </fieldset>
           </div>
-          <div className='md:col-span-6'>
+          <div className="md:col-span-6">
             <fieldset className="fieldset">
               <legend className="fieldset-legend">Chef</legend>
-              <input type="text" name="chef" className="input w-full" placeholder="Enter Coffee Shef" />
+              <input
+                type="text"
+                name="chef"
+                className="input w-full"
+                placeholder="Enter Coffee Shef"
+              />
             </fieldset>
           </div>
-          <div className='md:col-span-6'>
+          <div className="md:col-span-6">
             <fieldset className="fieldset">
               <legend className="fieldset-legend">Supplier</legend>
-              <input type="text" name="supplier" className="input w-full" placeholder="Enter Coffee Supplyer" />
+              <input
+                type="text"
+                name="supplier"
+                className="input w-full"
+                placeholder="Enter Coffee Supplyer"
+              />
             </fieldset>
           </div>
-          <div className='md:col-span-6'>
+          <div className="md:col-span-6">
             <fieldset className="fieldset">
               <legend className="fieldset-legend">Taste</legend>
-              <input type="text" name="taste" className="input w-full" placeholder="Enter coffee taste " />
+              <input
+                type="text"
+                name="taste"
+                className="input w-full"
+                placeholder="Enter coffee taste "
+              />
             </fieldset>
           </div>
-          <div className='md:col-span-6'>
+          <div className="md:col-span-6">
             <fieldset className="fieldset">
               <legend className="fieldset-legend">Category:</legend>
-              <input type="text" name="category" className="input w-full" placeholder="Enter coffee category  " />
+              <input
+                type="text"
+                name="category"
+                className="input w-full"
+                placeholder="Enter coffee category  "
+              />
             </fieldset>
           </div>
-          <div className='md:col-span-6'>
+          <div className="md:col-span-6">
             <fieldset className="fieldset">
               <legend className="fieldset-legend">Details:</legend>
-              <input type="text" name="details" className="input w-full" placeholder="Enter details" />
+              <input
+                type="text"
+                name="details"
+                className="input w-full"
+                placeholder="Enter details"
+              />
             </fieldset>
           </div>
 
-          <div className='md:col-span-12'>
+          <div className="md:col-span-12">
             <fieldset className="fieldset w-full">
               <legend className="fieldset-legend">Photo:</legend>
-              <input type="text" name="photo" className="input w-full" placeholder="Enter details" />
+              <input
+                type="text"
+                name="photo"
+                className="input w-full"
+                placeholder="Enter details"
+              />
             </fieldset>
           </div>
           <div className="md:col-span-12">
-            <button  className="btn w-full bg-[#632EE3] text-white cursor-pointer">Submit</button>
+            <button className="btn w-full bg-[#632EE3] text-white cursor-pointer">
+              Submit
+            </button>
           </div>
         </div>
       </form>
